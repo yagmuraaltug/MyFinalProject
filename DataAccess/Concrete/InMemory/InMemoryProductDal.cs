08f7339a7ea18d6f56fc158034bd3c101e4893f0
Yagmur <yagmuraaltug@gmail.com>
2021-02-17 18:43:56 +0300

    Add project files.

---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,67 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _products;
+        public InMemoryProductDal()
+        {
+            _products = new List<Product> {
+             new Product{ProductId=1, CategoryId=1, ProductName="Computer", UnitPrice=5000, UnitsInStock=10},
+             new Product{ProductId=2, CategoryId=1, ProductName="Phone", UnitPrice=7000, UnitsInStock=15},
+             new Product{ProductId=3, CategoryId=2, ProductName="Camera", UnitPrice=500, UnitsInStock=3},
+             new Product{ProductId=4, CategoryId=2, ProductName="TV", UnitPrice=3000, UnitsInStock=5},
+             new Product{ProductId=5, CategoryId=2, ProductName="Mouse", UnitPrice=85, UnitsInStock=8}
+            };
+        }
+
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            //LINQ - Language Integrated Query
+            //foreach (var p in _products)
+            //{
+              //  if(product.ProductId == p.ProductId)
+               // {
+                 //   productToDelete = p;
+                //}
+            //}
+            Product productToDelete = _products.SingleOrDefault(p=>p.ProductId == product.ProductId);
+
+            _products.Remove(productToDelete);
+
+        }
+
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+          return  _products.Where(p => p.CategoryId == categoryId).ToList();
+        }
+
+        public void UpDate(Product product)
+        {
+            //gonderdigim urun id sine sahip olan listedeki urunu bul
+            Product productToUpDate = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            productToUpDate.ProductName = product.ProductName;
+            productToUpDate.CategoryId = product.CategoryId;
+            productToUpDate.UnitPrice = product.UnitPrice;
+            productToUpDate.UnitsInStock = product.UnitsInStock;
+
+
+        }
+    }
+}
---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    //IEntity implement eden bir class bir veritabani tablosudurs
+    public interface IEntity
+    {
+
+    }
+}
